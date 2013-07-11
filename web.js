45bdbfb7dc9e923dcc80c3a32a8b3c10adc65700
Karthik Ramesh <karthikramesh@icloud.com>
2013-07-11 07:41:30 +0000

    Modified the web.js file

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+ var message = fs.readFileSync("index.html").toString()
+ response.send(message);
 });
 
 var port = process.env.PORT || 5000;
